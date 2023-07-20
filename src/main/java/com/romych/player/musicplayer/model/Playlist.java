c1bae3c2fbd9ce07d1d2b4b14a0d562b404fe103
romych <lutsiv55@gmail.com>
2023-07-20 11:36:04 +0300

    Create playlist and song models.

---
+++ src/main/java/com/romych/player/musicplayer/model/Playlist.java
@@ -0,0 +1,63 @@
+package com.romych.player.musicplayer.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Playlist {
+    private int id;
+    private String title;
+    private Song currentSong;
+    private List<Song> songs;
+
+    private LocalDate creatingDate;
+
+    public Playlist() {
+    }
+
+    public Playlist(String title, Song currentSong, List<Song> songs, LocalDate creatingDate) {
+        this.title = title;
+        this.currentSong = currentSong;
+        this.songs = songs;
+        this.creatingDate = creatingDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Song getCurrentSong() {
+        return currentSong;
+    }
+
+    public void setCurrentSong(Song currentSong) {
+        this.currentSong = currentSong;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    public LocalDate getCreatingDate() {
+        return creatingDate;
+    }
+
+    public void setCreatingDate(LocalDate creatingDate) {
+        this.creatingDate = creatingDate;
+    }
+}
---
+++ src/main/java/com/romych/player/musicplayer/model/Song.java
@@ -0,0 +1,39 @@
+package com.romych.player.musicplayer.model;
+
+public class Song {
+    private int id;
+    private String path;
+    private Playlist playlist;
+
+    public Song() {
+    }
+
+    public Song(String path, Playlist playlist) {
+        this.path = path;
+        this.playlist = playlist;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Playlist getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(Playlist playlist) {
+        this.playlist = playlist;
+    }
+}
